131650e9df7f04c2e1db5dbe24cc1afee3b2545d
Tyler Bushnell <tyler@interlincx.com>
2015-12-02 11:48:01 -0800

    confirm working

--- example/index.js
+++ example/index.js
@@ -3,7 +3,7 @@
 
 var SimpleSignup = require('..')
 
-var ss = new SimpleSignup({server:'http://localhost:3023'})
+var ss = SimpleSignup({server:'http://localhost:3023'})
 
 init()
 
@@ -21,14 +21,54 @@ function runRoutes (el) {
   el.innerHTML = ''
 
   if (appState === 'signup') return signupRoute(el)
-  //if (appState === 'confirm') return confirmRoute(el)
+  if (appState.match(/^confirm/)) return confirmRoute(el, appState)
+  if (appState === 'protected') return protectedRoute(el)
   //if (appState === 'login') return loginRoute(el)
   //if (appState === 'logout') return logoutRoute(el)
+
+  return signupRoute(el)
 
 }
 
 function signupRoute (el) {
-  var form = ss.signup()
+  var urlTemplate = window.location.origin + '#/confirm/<%= email %>/<%= confirmToken %>'
+  var bodyTemplate = [
+    '<h1>Welcome to Example</h1>',
+    '<p>Thanks for signing up! Please ',
+      '<a href="' + urlTemplate + '">confirm your account</a> ',
+      'to continue.',
+    '</p>'
+  ].join('')
+
+  var opts = {
+    bodyTemplate: bodyTemplate,
+    from: 'Example Signup <example@signup.com>',
+    subject: 'Welcome!'
+  }
+
+  var form = ss.signup(opts)
   el.appendChild(form)
 }
 
+function confirmRoute (el, appState) {
+  console.log("CONFIRMING")
+  var paths = appState.split('/')
+  var opts = {
+    email: paths[1],
+    confirmToken: paths[2]
+  }
+  var conf = ss.confirm(opts, onLogin)
+  el.appendChild(conf)
+}
+
+function onLogin () {
+  window.location.hash = '/protected'
+}
+
+function protectedRoute (el) {
+  if (ss.authToken) {
+    el.innerHTML = "You're logged in"
+  } else {
+    el.innerHTML = "Not logged in!"
+  }
+}
